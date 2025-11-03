5afe78ca8a141cf5084968ad1c20b083e1c3bcfc
tamminenibhavya <160052000+tamminenibhavya@users.noreply.github.com>
2025-11-03 10:44:26 +0000

    logging mechanism

--- bundle/src/orchestrator.py
+++ bundle/src/orchestrator.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 
 import dlt
+import logging
 
 from extract_data import extract_turbine_data
 from pyspark.sql import SparkSession
@@ -8,6 +9,12 @@ from transform_data import create_turbine_cleansed_data_silver, create_turbine_d
 from load_data import load_to_gold
 
 spark = SparkSession.builder.getOrCreate()
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
 
 catalog = spark.conf.get("catalog_name") # type: ignore
 schema = spark.conf.get("schema_name") # type: ignore
@@ -17,11 +24,30 @@ volume_name = spark.conf.get("volume_name") # # type: ignore
 volume_path = f"/Volumes/{catalog}/{schema}/{volume_name}/"
 
 # Extract
-extract_turbine_data(spark, volume_path)
+logging.info(f"Starting extraction from {volume_path}")
+try:
+    extract_turbine_data(spark, volume_path)
+    logging.info("Data extraction completed successfully.")
+except Exception as e:
+    logging.error(f"Error during data extraction: {e}")
+    
 
 # Transform
-create_turbine_cleansed_data_silver(spark)
-create_turbine_data_gold(spark)
+try:
+    logging.info("Starting transformation to create cleansed data.")
+    create_turbine_cleansed_data_silver(spark)
+    logging.info("Cleansed data transformation completed successfully.")
+    
+    logging.info("Starting transformation to create gold data.")
+    create_turbine_data_gold(spark)
+    logging.info("Gold data transformation completed successfully.")
+except Exception as e:
+    logging.error(f"Error during data transformation: {e}")
 
 # Load
-load_to_gold(spark)
+try:
+    logging.info("Starting loading data to gold.")
+    load_to_gold(spark)
+    logging.info("Data loading completed successfully.")
+except Exception as e:
+    logging.error(f"Error during data loading: {e}")
